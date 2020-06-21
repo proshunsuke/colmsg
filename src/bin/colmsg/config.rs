6bd0a14f24c18c7978f58aa20e540b2c79e9e463
proshunsuke <shunsuke0901@gmail.com>
2020-06-21 12:17:18 +0900

    suport v2.0.00

--- src/bin/colmsg/config.rs
+++ src/bin/colmsg/config.rs
@@ -1,11 +1,14 @@
 use std::env;
 use std::ffi::OsString;
 use std::fs;
+use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
 
 use shell_words;
 
 use colmsg::dirs::PROJECT_DIRS;
+use colmsg::{errors::*, http};
 
 pub fn config_file() -> PathBuf {
     env::var("COLMSG_CONFIG_PATH")
@@ -15,7 +18,7 @@ pub fn config_file() -> PathBuf {
         .unwrap_or_else(|| PROJECT_DIRS.config_dir().join("config"))
 }
 
-pub fn get_args_from_config_file() -> Result<Vec<OsString>, shell_words::ParseError> {
+pub fn get_args_from_config_file() -> Result<Vec<OsString>> {
     Ok(fs::read_to_string(config_file())
         .ok()
         .map(|content| get_args_from_str(&content))
@@ -23,17 +26,39 @@ pub fn get_args_from_config_file() -> Result<Vec<OsString>, shell_words::ParseEr
         .unwrap_or_else(|| vec![]))
 }
 
-fn get_args_from_str(content: &str) -> Result<Vec<OsString>, shell_words::ParseError> {
+fn get_args_from_str(content: &str) -> Result<Vec<OsString>> {
     let args_par_line = content
         .split('\n')
         .map(|line| line.trim())
         .filter(|line| !line.is_empty())
         .filter(|line| !line.starts_with('#'))
         .map(|line| shell_words::split(line))
-        .collect::<Result<Vec<_>, _>>()?;
+        .collect::<Vec<_>>();
     Ok(args_par_line
         .iter()
         .flatten()
+        .flatten()
         .map(|line| line.into())
-        .collect())
+        .collect::<Vec<_>>())
+}
+
+pub fn get_access_token_from_file(refresh_token: &String) -> Result<String> {
+    let dir = PROJECT_DIRS.config_dir().to_path_buf();
+    if !dir.is_dir() { fs::create_dir_all(&dir)? };
+    let file = dir.join("access_token");
+    if file.is_file() {
+        Ok(fs::read_to_string(file)?)
+    } else {
+        let update_token_res = http::update_token::request(refresh_token)?;
+        let mut f = File::create(file)?;
+        f.write_all(update_token_res.access_token.as_ref())?;
+        Ok(update_token_res.access_token)
+    }
+}
+
+pub fn delete_access_token_file() -> Result<()> {
+    let dir = PROJECT_DIRS.config_dir().to_path_buf();
+    let file = dir.join("access_token");
+    if !file.is_file() { return Ok(()); }
+    Ok(fs::remove_file(file)?)
 }
--- src/http/client.rs
+++ src/http/client.rs
@@ -2,10 +2,13 @@ use std::env;
 
 use reqwest::blocking::Client as reqwest_client;
 use reqwest::blocking::Response;
-use reqwest::header::{HeaderMap, CONTENT_TYPE, ACCEPT_LANGUAGE, USER_AGENT, CONNECTION, ACCEPT_ENCODING};
+use reqwest::header::{HeaderMap, CONTENT_TYPE, ACCEPT_LANGUAGE, USER_AGENT, CONNECTION, ACCEPT_ENCODING, TE, AUTHORIZATION, ACCEPT};
 use serde::Serialize;
 use serde::de::DeserializeOwned;
 use url::Url;
+
+#[cfg(feature = "401")]
+use rand::Rng;
 
 use crate::errors::*;
 use crate::reqwest;
@@ -28,7 +31,7 @@ impl Client {
         let url = Url::parse(&self.base_url).unwrap().join(&path).unwrap();
         let response: Response = self.client
             .post(url)
-            .headers(self.header())
+            .headers(self.insert_headers(HeaderMap::new()))
             .json(json)
             .send()?;
         let body = response.text()?;
@@ -44,29 +47,69 @@ impl Client {
         }
     }
 
-    pub fn get_request(&self, url: &str) -> Result<Response> {
-        Ok(
-            self.client
+    pub fn get_request<RT>(&self, path: &str, access_token: &str, parameters: Option<Vec<(&str, &str)>>) -> Result<RT>
+        where RT: DeserializeOwned {
+        let mut header = self.insert_headers(HeaderMap::new());
+        header = self.insert_optional_headers(header, access_token);
+
+        let iter = match parameters {
+            Some(v) => v,
+            None => vec![]
+        };
+        let url = Url::parse(&self.base_url).unwrap().join(&path).unwrap();
+        let url = Url::parse_with_params(url.as_str(), &iter).unwrap();
+        let url_str = url.as_ref().to_string();
+        let response: Response = self.client
             .get(url)
-            .headers(self.header())
+            .headers(header)
             .send()?
-        )
+            .error_for_status()?;
+        let body = response.text()?;
+        let result = serde_json::from_str::<RT>(&body);
+        match result {
+            Ok(t) => Ok(t),
+            Err(e) => {
+                let error_message = format!(
+                    "error: {}, request url: {}, response body: {}", e.to_string(), &url_str, &body
+                );
+                Err(error_message.into())
+            }
+        }
     }
 
-    fn header(&self) -> HeaderMap {
-        let mut subscribe_list_headers = HeaderMap::new();
-        subscribe_list_headers.insert(CONTENT_TYPE, "application/json".parse().unwrap());
-        subscribe_list_headers.insert("X-API-Version", "1.7.0".parse().unwrap());
-        subscribe_list_headers.insert(ACCEPT_LANGUAGE, "ja-JP".parse().unwrap());
-        subscribe_list_headers.insert(USER_AGENT, "Dalvik/2.1.0 (Linux; U; Android 6.0; Samsung Galaxy S7 for keyaki messages Build/MRA58K)".parse().unwrap());
-        subscribe_list_headers.insert(CONNECTION, "Keep-Alive".parse().unwrap());
-        subscribe_list_headers.insert(ACCEPT_ENCODING, "gzip".parse().unwrap());
-        subscribe_list_headers
+    fn insert_headers(&self, mut header: HeaderMap) -> HeaderMap {
+        header.insert(ACCEPT, "application/json".parse().unwrap());
+        header.insert(CONTENT_TYPE, "application/json".parse().unwrap());
+        header.insert("X-Talk-App-ID", "jp.co.sonymusic.communication.keyakizaka 2.0".parse().unwrap());
+        header.insert(ACCEPT_LANGUAGE, "ja-JP".parse().unwrap());
+        header.insert(USER_AGENT, "Dalvik/2.1.0 (Linux; U; Android 6.0; Samsung Galaxy S7 for keyaki messages Build/MRA58K)".parse().unwrap());
+        header.insert(CONNECTION, "Keep-Alive".parse().unwrap());
+        header.insert(ACCEPT_ENCODING, "gzip".parse().unwrap());
+        header.insert(TE, "gzip, deflate; q=0.5".parse().unwrap());
+        header
     }
+
+    fn insert_optional_headers(&self, mut header: HeaderMap, access_token: &str) -> HeaderMap {
+        let authorization = format!("Bearer {}", access_token);
+        header.insert(AUTHORIZATION, authorization.parse().unwrap());
+        header = self.insert_401_header(header);
+        header
+    }
+
+    #[cfg(feature = "401")]
+    fn insert_401_header(&self, mut header: HeaderMap) -> HeaderMap {
+        if rand::thread_rng().gen_range(0, 6) == 0 {
+            header.insert("Prefer", "code=401".parse().unwrap());
+        };
+        header
+    }
+
+    #[cfg(not(feature = "401"))]
+    fn insert_401_header(&self, header: HeaderMap) -> HeaderMap { header }
 }
 
 fn base_url() -> String {
     env::var("BASE_URL")
         .ok()
-        .unwrap_or_else(|| "https://client-k.hot.sonydna.com".to_string())
+        .unwrap_or_else(|| "https://api.kh.glastonr.net".to_string())
 }
