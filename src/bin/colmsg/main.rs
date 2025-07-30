7295281bfdc24a3c18ea54987558beec339b2efd
kyutaze <215492008+kyutaze@users.noreply.github.com>
2025-07-30 12:55:40 +0900

    add basic support for Mai Shiraishi Message app

--- src/bin/colmsg/main.rs
+++ src/bin/colmsg/main.rs
@@ -15,7 +15,7 @@ use colmsg::dirs::PROJECT_DIRS;
 use colmsg::{errors::*, Config};
 use colmsg::errors::ErrorKind::ReqwestError;
 use colmsg::controller::Controller;
-use colmsg::http::client::{SClient, SHNClient, HClient, NClient, AClient};
+use colmsg::http::client::{SClient, SHNClient, HClient, NClient, AClient, MClient};
 
 fn run_controller<C: SHNClient>(config: &Config<C>) -> Result<bool> {
     let controller = Controller::new(config);
@@ -63,6 +63,17 @@ fn run_asukasaito(app: &App) -> Result<bool> {
     };
     if !is_run_by_group { return Ok(true) };
     let config: Config<AClient> = app.asukasaito_config()?;
+    run_controller(&config)
+}
+
+fn run_maishiraishi(app: &App) -> Result<bool> {
+    if let None = app.matches.value_of("m_refresh_token") { return Ok(true) };
+    let is_run_by_group = match app.matches.values_of("group") {
+        Some(k) => k.clone().any(|v| v == "maishiraishi"),
+        None => true
+    };
+    if !is_run_by_group { return Ok(true) };
+    let config: Config<MClient> = app.maishiraishi_config()?;
     run_controller(&config)
 }
 
@@ -125,6 +136,20 @@ fn run() -> Result<bool> {
                 if Some(StatusCode::UNAUTHORIZED) != re.status() { break; };
                 delete_access_token_file()?;
                 result = run_asukasaito(&app);
+            }
+            _ => { break; }
+        }
+    }
+
+    if let Err(_e) = &result { return result; }
+
+    let mut result = run_maishiraishi(&app);
+    loop {
+        match &result {
+            Err(Error(ReqwestError(re), _)) => {
+                if Some(StatusCode::UNAUTHORIZED) != re.status() { break; };
+                delete_access_token_file()?;
+                result = run_maishiraishi(&app);
             }
             _ => { break; }
         }
